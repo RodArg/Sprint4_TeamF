0246be3e18c152555a36a5f5ff56a15aa5607eec
Shrina Parikh <sp4681@nyu.edu>
2020-08-04 15:37:25 -0400

    barebones site

---
+++ app.js
@@ -0,0 +1,34 @@
+//https://github.com/thelinmichael/spotify-web-api-node/blob/master/src/spotify-web-api.js
+const express = require('express');
+
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false })); 
+
+//create table object
+//create transactin form
+
+
+
+
+app.get('/', function(req, res) {
+    res.render('login');
+});
+
+app.get('/budgeting', function(req,res) {
+  res.render('budgeting'); //pass transaction form as an argument here
+});  
+
+app.post('/budgeting', function(req,res) {
+  res.render('budgeting'); //pass transaction form as an argument here
+}); //handle form submission data here
+
+app.listen(3000);
+
+
+
+
+
