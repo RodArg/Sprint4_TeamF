140dc8001fa5b0036427cbe93862219a8f685ccd
sydneyq <qrsydney@gmail.com>
2020-08-04 18:11:33 -0400

    add get_total

--- TextExtraction/pytess_extract.py
+++ TextExtraction/pytess_extract.py
@@ -21,10 +21,38 @@ def get_text(images):
             receipts.append(img_text)
             print(img_name, ":")
             print(img_text)
+            get_total(img_text)
             print("-"*20)
         except:
             print(img_name, " image object unsupported")
     return receipts
 
+
+def get_total(img_text) -> float:
+    split = img_text.split()
+
+    # testing prints
+    print("-" * 20)
+    print('SPLIT HERE:')
+    print(split)
+    print("-" * 20)
+
+    for item in reversed(split):
+        # we identify the total amount by checking for
+        # a $ and a decimal number
+        if '.' in item and '$' in item:
+            print(f'found item: {item}')
+            # get rid of all characters that are not a number or '.'
+            # then return the cleaned total
+            total = ''
+            for c in item:
+                print(f'--- loop: {c}')
+                if c == '.' or (ord(c) > 47 and ord(c) < 58):
+                    print(f'--- loop: add {c}')
+                    total += c
+            total = round(float(total), 2)
+            print(f'total: {total}')
+            return total
+    return -1
 
 print(os.listdir(os.getcwd()+"/images"))
