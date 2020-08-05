3b0fe86069468cd4660e8bb3e55794eaa3c563e3
Rodrigo Arguello <rodrigo.arg01@gmail.com>
2020-08-05 15:48:46 -0500

    Added translation, get language, and date parsing for googletranslate languages. Cleaned up comments

--- TextExtraction/pytess_extract.py
+++ TextExtraction/pytess_extract.py
@@ -1,17 +1,56 @@
 import cv2
 import pytesseract
 import os
+from statistics import mode
+from googletrans import Translator
 
 path = os.getcwd()
-img_folder = os.listdir(path+"/TextExtraction/images")
+img_folder = os.listdir(path+"/TextExtraction/images") # Use this line when executing outside TextExtraction
+# img_folder = os.listdir(path+"/images") # Use this line when running from IDE
 
-# Single hardcoded file test
-# img = cv2.imread("images/text1.png")
-# text = pytesseract.image_to_string(img)
-# print("images/text1.png:", text)
+translator = Translator()
+
+def get_language(text):
+    """
+    Input: A single string of text in an identifiable language
+    Output: a two-char string language identifier (en = english, de = deutsch, etc.)
+    """
+    text = text.split()
+    text.reverse()
+    languages = translator.detect(text[:len(text) // 4])  # Parsing the whole text is slow so we only look at 1/4th
+
+    for i in range(len(languages)):
+        languages[i] = languages[i].lang  # languages[i] are language objects, .lang is a string
+    language = mode(languages)
+    print(f"language: {language}")
+    return language
+
+
+def translate_words(text, language=""):
+    """
+    Input:
+        text: List of strings
+        language: two-char string language symbol (en, fr, cn, etc) of source text
+    Output: list of strings translated to english
+    """
+    if(language == ""):
+        language = get_language(text)
+
+    text = text.split()
+    translations = translator.translate(text, src=language, dest='en')
+    translated_text = []
+
+    for translation in translations:
+        translated_text.append(translation.text)
+
+    return translated_text
 
 
 def get_text(images):
+    """
+    Input: A list of filenames of images to be converted to text
+    Output: A list of strings of images converted to text
+    """
     receipts = []
     for img_name in images:
         print("Looking at:", img_name)
@@ -24,19 +63,17 @@ def get_text(images):
 
             print(img_name, ":")
             print(img_text)
-            split = img_text.split()
-
-            print("-" * 20)  # Separator, delete if unnecessary
-            get_total(split)
-            print("-" * 20)
-            get_company(split)
-            print("-" * 20)
+            print("-"*20)  # Separator, delete if unnecessary
+            get_total(img_text)
+            print("-"*20)
         except:
             print(img_name, " image object unsupported")
     return receipts
 
 
-def get_total(split) -> float:
+def get_total(img_text) -> float:
+    split = img_text.split()
+
     # testing prints
     print("-" * 20)
     print('SPLIT HERE:')
@@ -44,7 +81,8 @@ def get_total(split) -> float:
     print("-" * 20)
 
     for item in reversed(split):
-        # we identify the total amount by checking for the last '.' token
+        # we identify the total amount by checking for
+        # a $ and a decimal number
         if '.' in item:
             print(f'found item: {item}')
             # get rid of all characters that are not a number or '.'
@@ -52,43 +90,10 @@ def get_total(split) -> float:
             total = ''
             for c in item:
                 print(f'--- loop: {c}')
-                if c == '.' or (47 < ord(c) < 58):
+                if c == '.' or (ord(c) > 47 and ord(c) < 58):
                     print(f'--- loop: add {c}')
                     total += c
             total = round(float(total), 2)
             print(f'total: {total}')
             return total
     return -1
-
-
-def get_company(split) -> str:
-    # we identify a company name by checking from the top
-    # for letters until we read in a character that is not a letter
-    company = []
-    # we keep a boolean to see if we've already started
-    # seeing the company name
-    hit = False
-    for item in split:
-        item = item.replace(':', '')
-        # if we haven't seen the company name yet
-        # and we hit an alpha char
-        # then add it to the company name
-        if not hit:
-            if item.isalpha():
-                hit = True
-                company.append(item)
-        # if we have already started building the company name
-        # then check if this item is non alpha
-        # if it isn't, then we break
-        # otherwise, we add it to the company name
-        else:
-            if not item.isalpha():
-                break
-            else:
-                company.append(item)
-    company = " ".join(company)
-    print(f'company: {company}')
-    return company
-
-
-print(os.listdir(os.getcwd() + "/images"))
