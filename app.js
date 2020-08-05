f0e10541523aae1cb1de87a76806f96316ad3c6b
Shrina Parikh <sp4681@nyu.edu>
2020-08-05 09:14:57 -0400

    more login page and sidebar elements

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-//https://github.com/thelinmichael/spotify-web-api-node/blob/master/src/spotify-web-api.js
-const express = require('express');
 
+const express = require('express');
 const path = require('path');
 const app = express();
 
@@ -8,22 +7,37 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: false })); 
 
-//create table object
-//create transactin form
+//temp hardcoding transaction data
+class transaction {
+	constructor(date, vendor, amount) {
+		this.date = date;
+		this.vendor = vendor;
+		this.amount = amount;
+	}
+}
 
+const transArr = [];
+const trans1 = new transaction(new Date(2020, 08, 01), 'Wawa', 5.02);
+const trans2 = new transaction(new Date(2020, 08, 01), 'Shop Rite', 43.64);
+const trans3 = new transaction(new Date(2020, 08, 03), 'CVS', 12.99);
+transArr.push(trans1, trans2, trans3);
 
-
-
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { //for a fake auth
+	if(req.query.password === 'pat') {
+		res.redirect('/budgeting');
+	}
+	else {
+		res.render('login', {errorMessage: "Incorrect login. Try again."});
+	}
     res.render('login');
 });
 
 app.get('/budgeting', function(req,res) {
-  res.render('budgeting'); //pass transaction form as an argument here
+  res.render('budgeting', {transactionData: transArr}); //pass transaction form as an argument here
 });  
 
 app.post('/budgeting', function(req,res) {
-  res.render('budgeting'); //pass transaction form as an argument here
+  res.render('budgeting', {transactionData: transArr}); //pass transaction form as an argument here
 }); //handle form submission data here
 
 app.listen(3000);
